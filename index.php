66b5b1bd7ddccaf83f027e15f69450096e20e64e
Agus Setiono <agussetiono326@gmail.com>
2020-09-27 22:03:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quisquam, alias veniam quaerat recusandae nobis, libero blanditiis commodi, est voluptatibus earum tempore! Adipisci dicta quas quo quae cum, explicabo optio.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
